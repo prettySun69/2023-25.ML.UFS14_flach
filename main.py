84c041f7adbe6e63d21f247bd31297d02c96d02f
Flavio <flavio.manna@itsrizzoli.it>
2024-09-24 15:43:41 +0000

    commit funzionante con output

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Ciao')
+import os
+
+model_dir = os.environ['SM_MODEL_DIR']
+
+with open(model_dir + '/output.txt', 'w') as f:
+    f.write('Ciao')
+    
+output_dir = os.environ['SM_OUTPUT_DIR']
+with open(output_dir + '/output.txt', 'w') as f:
+    f.write('Ciao')
